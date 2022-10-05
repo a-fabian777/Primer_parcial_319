c4fd0f5baa923d0605a3f3e17b53fc2378e96fd3
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-10-05 09:15:30 -0500

    Update Ejercicio7.py

--- Ejercicio7/Ejercicio7.py
+++ Ejercicio7/Ejercicio7.py
@@ -1,1 +1,19 @@
+def suma(a, b):
+        return a + b
 
+def resta(a, b):
+        return a - b
+
+def producto(a, b):
+        return a*b
+
+def dividir(a, b):
+        return a / b
+
+if __name__ == "__main__":
+        a = int(input("ingrese primer numero: "))
+        b = int(input("ingrese segundo numero: "))
+        print("La suma es: ", suma(a, b))
+        print("Las resta es: ", resta(a, b))
+        print("El producto es: ", producto(a, b))
+        print("La division es: ", dividir(a, b))
