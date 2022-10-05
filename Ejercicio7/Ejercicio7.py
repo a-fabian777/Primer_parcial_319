1052a817dd4153096cae455f40b6fb741629d646
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-10-05 11:31:01 -0500

    Update Ejercicio7.py

--- Ejercicio7/Ejercicio7.py
+++ Ejercicio7/Ejercicio7.py
@@ -13,7 +13,14 @@ def dividir(a, b):
 if __name__ == "__main__":
         a = int(input("ingrese primer numero: "))
         b = int(input("ingrese segundo numero: "))
-        print("La suma es: ", suma(a, b))
-        print("Las resta es: ", resta(a, b))
-        print("El producto es: ", producto(a, b))
-        print("La division es: ", dividir(a, b))
+        print("ELIJA UNA DE LAS OPERACION")
+        print("1.- Suma")
+        print("2.- Resta")
+        print("3.- Producto")
+        print("4.- Division")
+        op = int(input())
+        if op ==1 : print("La suma es: ", suma(a, b))
+        elif op == 2 : print("Las resta es: ", resta(a, b))
+        elif op == 3 : print("El producto es: ", producto(a, b))
+        elif op == 4 : print("La division es: ", dividir(a, b))
+
