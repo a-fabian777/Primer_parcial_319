9295ee5643dd81a66bcc6c7bc1eac3078cddb4c3
a_fabian_alcon <73066810+a-fabian777@users.noreply.github.com>
2022-10-05 11:36:28 -0500

    Update Ejercicio7.py

--- Ejercicio7/Ejercicio7.py
+++ Ejercicio7/Ejercicio7.py
@@ -19,8 +19,16 @@ if __name__ == "__main__":
         print("3.- Producto")
         print("4.- Division")
         op = int(input())
-        if op ==1 : print("La suma es: ", suma(a, b))
-        elif op == 2 : print("Las resta es: ", resta(a, b))
-        elif op == 3 : print("El producto es: ", producto(a, b))
-        elif op == 4 : print("La division es: ", dividir(a, b))
+        if op ==1 :
+                print("La suma es: ", suma(a, b))
+        elif op == 2 :
+                print("Las resta es: ", resta(a, b))
+        elif op == 3 :
+                print("El producto es: ", producto(a, b))
+        elif op == 4 :
+                if b == 0:
+                        print("no se puede dividir entre 0!!!")
+                else:
+                        print("La division es: ", dividir(a, b))
+
 
